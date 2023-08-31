054fe4a6eac649f815542afbd8f304813c9ace29
dchen <dchen@tripadvisor.com>
2023-08-31 23:53:44 +1000

    modify

--- typescript-course/c1/app.js
+++ typescript-course/c1/app.js
@@ -1,7 +1,44 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, resultType) {
+    var result;
+    if (typeof n1 === 'number' || resultType == 'asNumber') {
+        result = +n1 + +n2;
+    }
+    else {
+        result = number1.toString() + n2;
+    }
+    return result;
 }
-var number1 = '5';
+var number1 = 5;
 var number2 = 2.5;
-var result = add(number1, number2);
+var printResult = true;
+var Role;
+(function (Role) {
+    Role[Role["MANAGER"] = 10] = "MANAGER";
+    Role["ADMIN"] = "Admin";
+    Role[Role["DEV"] = 11] = "DEV";
+})(Role || (Role = {}));
+;
+var person = {
+    name: 'Deven',
+    age: 40,
+    hobbies: ['sport'],
+    role: Role.MANAGER
+};
+person.role[0] = 10;
+var result = add(number1, number2, 'asNumber');
 console.log(result);
+var re1 = add('abc', 123, 'asString');
+function printTextResult(text) {
+    console.log('I am ' + text);
+}
+var fa = add;
+var fa1;
+// fa1 = fa; //fail
+var fa2;
+fa2 = function (a, b) { return fa1(a, b).toString(); };
+printTextResult(fa('123', 123, 'asString'));
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(10, 20, function (result) { return printTextResult(result.toString()); });
---
+++ typescript-course/resources/README.md
@@ -0,0 +1,5 @@
+Attached you find all the code snapshots for this module - you also find them attached to individual lectures throughout this module.
+
+These links might also be interesting:
+
+Official TypeScript Docs: https://www.typescriptlang.org/docs/handbook/2/everyday-types.html
